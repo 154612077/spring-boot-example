ac4f459eea4147e742cff45e240e48339ca89b12
xzk <154612077@qq.com>
2022-03-25 11:55:01 +0800

    1

--- src/main/java/springboot/example/Application.java
+++ src/main/java/springboot/example/Application.java
@@ -12,7 +12,7 @@ public class Application {
     @GetMapping
     public String home(String name){
         System.out.println("姓名：" + name);
-        System.out.println("姓名123：" + name);
+        System.out.println("姓名：" + name);
         return "hello" + name;
     }
 
